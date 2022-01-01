3110702705b2549c6f8f254c0c4e4c5d269738f5
joelgroomer <joelgroomer@users.noreply.github.com>
2021-12-31 18:25:25 -0800

    Added restrictions on tracking stocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :user_stocks
+  resources :user_stocks, only: [:create]
   devise_for :users
   root 'pages#index'
   get 'my_portfolio', to: 'users#my_portfolio'
